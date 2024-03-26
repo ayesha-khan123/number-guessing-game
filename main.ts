d05f7ed23865cd147c93e253b51a7e5d3c862167
ayesha khan <160107761+ayesha-khan123@users.noreply.github.com>
2024-03-26 03:43:21 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+type ansType = {
+    userGuess: number
+}
+
+const systemGeneratedNo = Math.floor (Math.random() * 10);
+
+const answer: ansType =  await inquirer.prompt([
+    
+    {
+        type: "number",
+        name: "userGuess",
+        message: "write your gues b/w 1 to 10:"
+    },
+])
+
+const {userGuess} = answer;
+
+console.log(userGuess, "userGuess", systemGeneratedNo, 'SYs')
+if (userGuess === systemGeneratedNo){
+    
+    console.log("your answer is correct \n you win!")
+} else {
+    console.log("please try again good luck to your next time!");    
+}
+
